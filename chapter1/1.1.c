056cc5a9b75400ae6c584ea5447c8d48477f4c2b
duchengli <duchengli@hotmail.com>
2023-01-18 10:04:50 +0800

    20230118@office

--- chapter1/1.1.c
+++ chapter1/1.1.c
@@ -1,11 +1,13 @@
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
-    using namespace std;
-    int *arr = new int[5];
-    int *nums = new int[5]{1, 2, 3, 4, 5};
+    printf("%d\n", 1 + 2);
+    printf("%d\n", 3 - 4);
+    printf("%d\n", 5 * 6);
+    printf("%d\n", 8 / 4);
+    printf("%d\n", 8 / 5);
+    printf("%d\n", 8 / 5);
 
     return 0;
-    
 }
