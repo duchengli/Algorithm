539d041348c0082e68923eb0e1f715ff659a8ba9
duchengli <duchengli@hotmail.com>
2023-02-07 16:43:16 +0800

    20230207@office

---
+++ chapter1/expA1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    // double a;
+    // a = 11111;
+    // printf("%f\n", a * a);
+    // a = 111111;
+    // printf("%f\n", a * a);
+    // a = 111111111;
+    // printf("%f\n", a * a);
+    // return 0;
+    // printf("%f\n", sqrt(-10));
+    // printf("%f\n", 0.0 / 1.0);
+    printf("%d\n", 1 / 0);
+    return 0;
+}
